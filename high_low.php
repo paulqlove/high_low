6817204afed390cc04c0ed64a0e7caadbd1585f4
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-24 11:30:47 -0600

    final

--- high_low.php
+++ high_low.php
@@ -2,34 +2,37 @@
 
 
 $number = mt_rand(0, 100);
+$count = 1;
 
 do {
 	//ask user to guess a number
 	fwrite(STDOUT, 'Guess');
 	$user = trim(fgets(STDIN));
+	
 	//check users number vs computers number
 	if ($user == $number) {
 	 	
 	 	echo "Great Guess! \n";
+	 	
 	 
 	 }
 	 
 	elseif($user > $number) {
-		echo "$user to high \n";
+		echo "$user too high \n";
 	} 
 	
 	elseif ($user < $number) {
-		echo "$user to low \n";		
+		echo "$user too low \n";		
 	}
-
 	
-
+	
+ $count++;
 	  
 	 
 	
 } while ( $number != $user);
 
-
+echo "final count" . $count++ . PHP_EOL;
 
 
 
