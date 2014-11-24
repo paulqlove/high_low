0b30e9cc5e1ad4dad0466417e1e62a20183d6b67
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-24 15:11:40 -0600

    arg

--- high_low.php
+++ high_low.php
@@ -1,39 +1,46 @@
 <?php
 
+//Dump arg count
 
-$number = mt_rand(0, 100);
-$count = 1;
+//Dump agr vars
+var_dump($argc);
+// Exit with 0 errors
+var_dump($argv);
+echo $argv[1] . PHP_EOL;
+echo $argv[2] . PHP_EOL;
 
-do {
-	//ask user to guess a number
-	fwrite(STDOUT, 'Guess');
-	$user = trim(fgets(STDIN));
+$count = 0;
+
+		if (is_numeric($argv[1]) && is_numeric($argv[2]) && $argc == 3) {
+			$number = mt_rand($argv[1],$argv[2]);
+			do{
+			//ask user to guess a number
+			fwrite(STDOUT, 'Guess');
+			$user = trim(fgets(STDIN));
+			
+			//check users number vs computers number
+				if ($user == $number) {
+				 	
+				 	echo "Great Guess! \n";
+				 	echo "final count" . " " . $count++ . PHP_EOL;
+				 }
+				 
+				elseif($user > $number) {
+					echo "$user too high \n";
+				} 
+				
+				elseif ($user < $number) {
+					echo "$user too low \n";		
+				}
+		 		$count++;
+			  	
+		 	} while ( $number != $user);
+		}			
+		else {
+				echo "Please pick a number next time" . PHP_EOL;
+
+			}
+
+
 	
-	//check users number vs computers number
-	if ($user == $number) {
-	 	
-	 	echo "Great Guess! \n";
-	 	
-	 
-	 }
-	 
-	elseif($user > $number) {
-		echo "$user too high \n";
-	} 
-	
-	elseif ($user < $number) {
-		echo "$user too low \n";		
-	}
-	
-	
- $count++;
-	  
-	 
-	
-} while ( $number != $user);
-
-echo "final count" . $count++ . PHP_EOL;
-
-
-
 
